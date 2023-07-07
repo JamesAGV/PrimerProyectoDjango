14393580c67dbaae75f1f35d4ab774afa18757de
JamesAGV <jamgarciavi@unal.educ.o>
2023-07-07 11:36:37 -0500

    Se incluyen etiquetas de HTML en el return de la vista saludo.

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-
+#<>
 def saludo(request): #primera vista
-    return HttpResponse("Hola Mundo, soy James García")
+    return HttpResponse("<html><body><h1>Hola Mundo, soy James García</h1></body></html>")
 
 def despedida(request): #segunda vista
     return HttpResponse("Página de despedida!!!")
+
