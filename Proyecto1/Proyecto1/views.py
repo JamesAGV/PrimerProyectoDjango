d4393b300a90d3f5405d4272d834974a4a294b52
JamesAGV <jamgarciavi@unal.educ.o>
2023-09-22 20:42:11 -0500

    Cambio en el HttResponse

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 #<>
 def saludo(request): #primera vista
-    return HttpResponse("<html><body><h1>Hola Mundo, soy James García</h1></body></html>")
+    documento = "<html><body><h1>Hola Mundo, soy James García</h1></body></html>"
+    return HttpResponse(documento)
 
 def despedida(request): #segunda vista
-    return HttpResponse("Página de despedida!!!")
+    documento = "Página de despedida!!!"
+    return HttpResponse(documento)
 
